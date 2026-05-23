2a70883cf54e680417414e0e2f2595fdac5d41c5
srikar544 <koppalasrikar@gmail.com>
2026-05-23 15:31:14 +0530

    first commit

--- src/prompt.py
+++ src/prompt.py
@@ -3,7 +3,7 @@ system_prompt = (
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
-    "answer concise."
+    "answer concisely."
     "\n\n"
     "{context}"
 )
